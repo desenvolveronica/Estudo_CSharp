c864bca6dc3223b18fe25a4a70c40a7e4815b203
Verônica Helena <desenvolveronica@gmail.com>
2022-10-03 17:54:49 -0300

    Observações

--- Program.cs
+++ Program.cs
@@ -2,3 +2,17 @@
 Console.WriteLine("Hello, Diva!");
 //Em program.css fica o código fonte da aplicação
 //dotnet run => para compilar e executar
+Console.WriteLine("Para criar uma variável precisamos especificar o tipo dela");
+Console.WriteLine("Podemos usar a vírgula e criar mais de uma variável do mesmo tipo");
+Console.WriteLine("Para finalizar uma declaração precisamos usar OBRIGATORIAMENTE o ; ");
+//tipos:
+// bool;
+// char;
+// byte;
+// int;
+// long;
+// float;
+// double;
+// decimal;
+// string;
+
