9d4b82b001bf20d32119c409f4653bdafefa9d99
Verônica Helena <desenvolveronica@gmail.com>
2022-10-03 17:34:43 -0300

    dotnet run

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Diva!");
 //Em program.css fica o código fonte da aplicação
-
+//dotnet run => para compilar e executar
