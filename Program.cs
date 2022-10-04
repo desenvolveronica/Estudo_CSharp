caf0da6bcd70030577fb7742fd23171951e7f0d1
Verônica Helena <desenvolveronica@gmail.com>
2022-10-04 14:56:10 -0300

    WriteLine

--- Program.cs
+++ Program.cs
@@ -6,13 +6,7 @@ Console.WriteLine("Para criar uma variável precisamos especificar o tipo dela")
 Console.WriteLine("Podemos usar a vírgula e criar mais de uma variável do mesmo tipo");
 Console.WriteLine("Para finalizar uma declaração precisamos usar OBRIGATORIAMENTE o ; ");
 //tipos:
-// bool;
-// char;
-// byte;
-// int;
-// long;
-// float;
-// double;
-// decimal;
-// string;
-
+ bool teste = true;
+Console.WriteLine(teste);
+Console.WriteLine("No caso abaixo a variável C foi declarada e seu tipo também, mas nenhum valor foi atribuido");
+char c; 
