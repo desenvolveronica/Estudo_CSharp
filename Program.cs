37ad2b0ed3f04bdf0563d29a9dc6ff38bdbfe323
Verônica Helena <desenvolveronica@gmail.com>
2022-10-04 15:21:40 -0300

    Observações

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine("Hello, Diva!");
 //Em program.css fica o código fonte da aplicação
 //dotnet run => para compilar e executar
+Console.WriteLine("WriteLine é um método");
 Console.WriteLine("Para criar uma variável precisamos especificar o tipo dela");
 Console.WriteLine("Podemos usar a vírgula e criar mais de uma variável do mesmo tipo");
 Console.WriteLine("Para finalizar uma declaração precisamos usar OBRIGATORIAMENTE o ; ");
@@ -10,3 +11,10 @@ Console.WriteLine("Para finalizar uma declaração precisamos usar OBRIGATORIAME
 Console.WriteLine(teste);
 Console.WriteLine("No caso abaixo a variável C foi declarada e seu tipo também, mas nenhum valor foi atribuido");
 char c; 
+
+int value1 = 10;
+value1++;
+
+Console.WriteLine("O valor do número é : " + value1 );
+Console.WriteLine("Não podemos usar áspas simples");
+Console.WriteLine("Para concatenar usamos o +");
